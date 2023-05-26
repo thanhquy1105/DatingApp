fb653ab02a82af85bc7f47ca930b14c628ef99de
Thành Quý <thanhquy1105@gmail.com>
2023-05-27 01:06:18 +0700

    authentication Adding a login endpoint

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
 {
   [ApiController]
   [Route("api/[controller]")]
-  public class UsersController : ControllerBase
+  public class UsersController : BaseApiController
   {
     private readonly DataContext _context;
     public UsersController(DataContext context)
@@ -19,13 +19,15 @@ namespace API.Controllers
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-        return await _context.Users.ToListAsync();
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+      return await _context.Users.ToListAsync();
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUser(int id){
-        return await _context.Users.FindAsync(id);
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+      return await _context.Users.FindAsync(id);
     }
   }
 }
