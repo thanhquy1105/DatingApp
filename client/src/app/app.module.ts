d05efb1dc1c234463f19b5eec584771505666c32
Thành Quý <thanhquy1105@gmail.com>
2023-05-29 03:08:10 +0700

    injecting services into component to do login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
@@ -13,6 +14,7 @@ import { NavComponent } from './nav/nav.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  loggedIn: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.loggedIn = true;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
-
 }
