4330f903a74c66566959484fff60ba7398177f7e
Thành Quý <thanhquy1105@gmail.com>
2023-07-08 04:54:06 +0700

    reactive form and validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,13 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -9,14 +16,49 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm!: FormGroup;
   constructor(
     private accountService: AccountService,
     private toastr: ToastrService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initailizeForm();
+  }
+
+  initailizeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValues('password'),
+      ]),
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === (control.parent?.controls as any)[matchTo].value
+        ? null
+        : { notSame: true };
+    };
+  }
+
+  // matchValues: ValidatorFn = (
+  //   group: AbstractControl
+  // ): ValidationErrors | null => {
+  //   let pass = group.get('password')?.value;
+  //   let confirmPass = group.get('confirmPassword')?.value;
+  //   return pass === confirmPass ? null : { notSame: true };
+  // };
 
   register() {
+    console.log(this.registerForm?.value);
     this.accountService.register(this.model).subscribe({
       next: (res) => {
         console.log(res);
@@ -24,7 +66,7 @@ export class RegisterComponent implements OnInit {
       },
       error: (error) => {
         console.log(error);
-        this.toastr.error(error.error); 
+        this.toastr.error(error.error);
       },
     });
   }
