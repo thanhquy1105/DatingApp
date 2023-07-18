48a4f6a047c51f5e895548ca9cb86b688f76a48f
Thành Quý <thanhquy1105@gmail.com>
2023-07-18 23:05:39 +0700

    authenticating to SignalR

--- API/Startup.cs
+++ API/Startup.cs
@@ -53,7 +53,10 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
 
